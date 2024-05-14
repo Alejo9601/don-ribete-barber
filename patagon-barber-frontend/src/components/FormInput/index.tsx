21233c9df8ff4f0bf6cd9e0e1ff6cafce133f72c
Alejo9601 <alejandro96jm@gmail.com>
2024-05-14 17:00:31 -0300

    Made props optional

--- patagon-barber-frontend/src/components/FormInput/index.tsx
+++ patagon-barber-frontend/src/components/FormInput/index.tsx
@@ -1,26 +1,32 @@
 import { ChangeEvent } from 'react'
 
 export function FormInput({
+  type = 'text',
   name,
-  label,
+  label = '',
   placeholder,
-  required,
+  required = false,
+  maxLength,
   onChange
 }: {
-  name: string
-  label: string
-  placeholder: string
-  required: boolean
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void
+  type?: string
+  name?: string
+  label?: string
+  placeholder?: string
+  required?: boolean
+  maxLength?: number
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
 }) {
   return (
     <label className="flex flex-col" htmlFor={name}>
       {label}
       <input
         className="bg-blue-300 p-2 rounded-lg text-black font-semibold"
+        type={type}
         name={name}
         placeholder={placeholder}
         required={required}
+        maxLength={maxLength}
         onChange={onChange}
       />
     </label>
