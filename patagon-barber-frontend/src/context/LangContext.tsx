38cae2c2c441c221a192dee77f7a7c0da648ddc0
Alejo9601 <alejandro96jm@gmail.com>
2024-05-15 16:15:20 -0300

    Commented code on LangContext till it´s working

--- patagon-barber-frontend/src/context/LangContext.tsx
+++ patagon-barber-frontend/src/context/LangContext.tsx
@@ -1,17 +1,16 @@
-import { createContext, useState } from 'react';
-import ES from '../assets/Languages/ES.json';
-import EN from '../assets/Languages/EN.json';
+// import { createContext, useState } from 'react'
+// import ES from '../assets/Languages/ES.json'
 
-const LanguagesContext = createContext<JSON | undefined>(ES);
+// const LanguagesContext = createContext<JSON | undefined>(ES)
 
-function LanguageProvider({ children }) {
-  const [language, setLanguage] = useState(ES);
-  return (
-    <LanguagesContext.Provider value={(language, setLanguage)}>
-      {children}
-    </LanguagesContext.Provider>
-  );
-}
+// function LanguageProvider({ children }) {
+//   const [language, setLanguage] = useState(ES)
+//   return (
+//     <LanguagesContext.Provider value={(language, setLanguage)}>
+//       {children}
+//     </LanguagesContext.Provider>
+//   )
+// }
 
-export { LanguageProvider };
-export default LanguagesContext;
+// export { LanguageProvider }
+// export default LanguagesContext
