be515816266f1793634bcca0ecdbbbfb835a8c96
Alejo9601 <alejandro96jm@gmail.com>
2024-05-15 17:33:42 -0300

    Fixed images not showing properly on vercel deploy

--- patagon-barber-frontend/src/components/BusinessServices/index.tsx
+++ patagon-barber-frontend/src/components/BusinessServices/index.tsx
@@ -1,6 +1,11 @@
 import ServiceContainer from './ServiceContainer'
 import ServiceDescription from './ServiceDescription'
 import services from '../../assets/json/services.json'
+import Beard from '../../assets/images/beard.webp'
+import Haircut from '../../assets/images/haircut.webp'
+import Eyebrow from '../../assets/images/eyebrow.webp'
+
+const descImages = [Haircut, Beard, Eyebrow]
 
 function isOddIndex(index: number) {
   return index % 2 !== 0
@@ -16,7 +21,7 @@ const BusinessServices = () => {
               descHeading={service.descHeading}
               descParagraph={service.descParagraph}
               reversed={isOddIndex(index)}
-              descImage={service.descImage}
+              descImage={descImages[index]}
             ></ServiceDescription>
           </ServiceContainer>
         )
