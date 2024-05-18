f7b87df8f29a16c89ce45d0d71e316bcc57debad
Alejo9601 <alejandro96jm@gmail.com>
2024-05-17 22:24:56 -0300

    Added media queries

--- patagon-barber-frontend/src/components/PreFooter/index.tsx
+++ patagon-barber-frontend/src/components/PreFooter/index.tsx
@@ -3,12 +3,12 @@ import BackgroundDotsSVG from '../SVGIcons/BackgroundDotsSVG'
 
 export function PreFooter() {
   return (
-    <section className="bg-black relative my-20 overflow-hidden">
+    <section className="relative my-20 overflow-hidden">
       <div className="flex flex-col gap-5 py-28 items-center justify-center bg-orange-700 z-[2]">
-        <h2 className="font-extrabold text-white text-7xl z-[2]">
+        <h2 className="text-white  text-center text-5xl lg:text-7xl font-extrabold z-[2]">
           Reserva tu turno ahora
         </h2>
-        <p className="text-slate-950 text-2xl font-semibold z-[2]">
+        <p className="text-slate-950 text-center text-2xl font-semibold z-[2]">
           ¡ Tenemos los mejores precios !
         </p>
         <Button onClick={() => {}} additionalProps="text-xl z-[2]">
