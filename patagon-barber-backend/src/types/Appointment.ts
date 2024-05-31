a460d39e0b39c1bfd1487887b892e1b434650f18
Alejo9601 <alejandro96jm@gmail.com>
2024-05-31 16:59:44 -0300

    Deleted service from appointments interface

--- patagon-barber-backend/src/types/Appointment.ts
+++ patagon-barber-backend/src/types/Appointment.ts
@@ -1,10 +1,9 @@
 import { Client } from './Client'
-import { Service } from './Service'
 
 export interface Appointment {
   id: number
   client: Client | undefined
   date: string
   time: string
-  service: Service | undefined
+  status: string
 }
