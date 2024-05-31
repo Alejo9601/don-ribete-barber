4d3c6199436ce7be410aeb39ce90e9c14a8949d4
Alejo9601 <alejandro96jm@gmail.com>
2024-05-31 15:58:52 -0300

    Created Loader component

---
+++ patagon-barber-frontend/src/components/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+export function Loader() {
+  return (
+    <div className="w-14 h-14 absolute top-[50%] right-[50%]">
+      <div className="animate-loader rounded-[100%] border-4 border-l-transparent w-full h-full"></div>
+    </div>
+  )
+}
