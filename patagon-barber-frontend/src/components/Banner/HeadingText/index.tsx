a6465bfac55fadbc132800f6f0f8f9030e31a43e
Alejo9601 <alejandro96jm@gmail.com>
2024-05-31 17:44:10 -0300

    Created component

---
+++ patagon-barber-frontend/src/components/Banner/HeadingText/index.tsx
@@ -0,0 +1,12 @@
+export default function HeadingText() {
+  return (
+    <>
+      <h1 className="text-white text-5xl md:text-7xl font-extrabold">
+        Reserva tu cita online
+      </h1>
+      <p className="text-gray-300 text-sm md:text-xl mt-8">
+        ¡Agenda tu cita ahora con nuestro sistema de reservas en linea!
+      </p>
+    </>
+  )
+}
