d3872099d393c46fbb0280e2f4f9c96443797cbc
Alejo9601 <alejandro96jm@gmail.com>
2024-05-31 15:58:10 -0300

    Added Loader animation

--- patagon-barber-frontend/tailwind.config.js
+++ patagon-barber-frontend/tailwind.config.js
@@ -15,11 +15,16 @@ export default {
         slide_right: {
           '0%': { opacity: 0, transform: 'translateX(200px)' },
           '100%': { opacity: 1, transform: 'translateX(0px)' }
+        },
+        loader: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' }
         }
       },
       animation: {
         slide_left: 'slide_left 1s ease',
-        slide_right: 'slide_right 1s ease'
+        slide_right: 'slide_right 1s ease',
+        loader: 'loader 1s linear infinite'
       }
     }
   },
