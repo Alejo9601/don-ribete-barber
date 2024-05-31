e77fc1f20efa396969632fb01639854a5420fbc5
Alejo9601 <alejandro96jm@gmail.com>
2024-05-31 16:00:13 -0300

    Changed token DB

--- patagon-barber-backend/src/models/db_client.ts
+++ patagon-barber-backend/src/models/db_client.ts
@@ -3,5 +3,5 @@ import { createClient } from '@libsql/client'
 export const client = createClient({
   url: 'libsql://patagon-barber-alejo9601.turso.io',
   authToken:
-    'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTQ2ODY1ODksImlkIjoiNTExM2M1MTItNjM1ZC00NDdlLWE5YmQtNzYxZjEwYWU0MTgwIn0.2RYObSMC0cpp4Q45-8b4NnBZZDj8BMa8YVFkWnzGFZYUM9cwn7nH9xdXym6_rKVz5Qph0LV1ggitquOzANwqDA'
+    'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTcxODA2NDksImlkIjoiZDMzMjg5ZDItMTJmYy00ODYzLWE0NTQtZTk1YWNkNDViOWQzIn0.8fDa9RemSISDroyyuRpKUZvGtlZ_W6n2l1eaqRqDMqb3Q_qHPPzf-EeiE2wontTHGMSDJ4vY23UrVFdWBzJ_DQ'
 })
