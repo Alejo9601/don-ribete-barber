3ee790f5a9ee6ad45ae932f86fff4d54e8ec215c
Alejo9601 <alejandro96jm@gmail.com>
2024-06-01 14:45:59 -0300

    Modified z-index

--- patagon-barber-frontend/src/components/ModalWrapper/index.tsx
+++ patagon-barber-frontend/src/components/ModalWrapper/index.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function ModalWrapper({ children }: { children: any }) {
   return (
-    <div className="h-screen w-screen fixed top-0 right-0 z-[1] flex flex-col items-center justify-center bg-black bg-opacity-75">
+    <div className="h-screen w-screen fixed top-0 right-0 z-[2] flex flex-col items-center justify-center bg-black bg-opacity-75">
       {children}
     </div>
   )
