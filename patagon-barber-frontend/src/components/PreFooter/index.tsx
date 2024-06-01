ba6c671eefb412951ed31213fef2b415ef840592
Alejo9601 <alejandro96jm@gmail.com>
2024-06-01 14:46:39 -0300

    Implemented ReservationModal on PreFooter

--- patagon-barber-frontend/src/components/PreFooter/index.tsx
+++ patagon-barber-frontend/src/components/PreFooter/index.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react'
 import Button from '../Button'
+import ReservationModal from '../ReservationModal'
 import BackgroundDotsSVG from '../SVGIcons/BackgroundDotsSVG'
 
 export function PreFooter() {
+  const [showModal, setShowModal] = useState(false)
+
+  function handleOnClick() {
+    setShowModal(!showModal)
+  }
+
+  function closeReservationModal() {
+    setShowModal(false)
+  }
+
   return (
     <section className="relative my-20 overflow-hidden">
       <div className="flex flex-col gap-5 py-28 items-center justify-center bg-orange-700 z-[2]">
@@ -11,12 +23,16 @@ export function PreFooter() {
         <p className="text-slate-950 text-center text-2xl font-semibold z-[2]">
           ¡ Tenemos los mejores precios !
         </p>
-        <Button onClick={() => {}} additionalProps="text-xl z-[2]">
+        <Button onClick={handleOnClick} additionalProps="text-xl z-[2]">
           Reservar
         </Button>
       </div>
       <BackgroundDotsSVG />
       <div className="bg-slate-800 h-28 w-1/3 absolute right-0 top-0 rounded-bl-xl"></div>
+
+      {showModal ? (
+        <ReservationModal closeModal={closeReservationModal}></ReservationModal>
+      ) : null}
     </section>
   )
 }
