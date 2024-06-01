803f608ed57f4bd66a136461f25541f8d74988c3
Alejo9601 <alejandro96jm@gmail.com>
2024-06-01 12:10:03 -0300

    Decentralized reservation process so it can be used anywhere

--- patagon-barber-frontend/src/components/Banner/BookAppointment/index.tsx
+++ patagon-barber-frontend/src/components/Banner/BookAppointment/index.tsx
@@ -1,35 +1,13 @@
-import { useContext, useState } from 'react'
 import Button from '../../Button'
-import ReservationModal from '../../ReservationModal'
-import { setAppointment } from '../../../services/appointmentServices'
-import { ReservationStatusModal } from '../../ReservationStatusModal'
-import { ReservationContext } from '../../../context/reservationContext'
-import { Loader } from '../../Loader/Loader'
-import { ModalWrapper } from '../../ModalWrapper'
 import HeadingText from '../HeadingText'
-
-enum reservationStates {
-  'STORING',
-  'FINISHED'
-}
+import ReservationModal from '../../ReservationModal'
+import { useState } from 'react'
 
 export default function BookAppointment() {
   const [showModal, setShowModal] = useState(false)
-  const [status, setStatus] = useState<reservationStates | null>(null)
-  const reservation = useContext(ReservationContext)
 
   function handleOnClick() {
     setShowModal(!showModal)
-  }
-
-  function onReservationFormSubmit() {
-    if (reservation?.appointment === undefined) return
-
-    setStatus(reservationStates.STORING)
-    setAppointment(reservation.appointment).then(() => {
-      setStatus(reservationStates.FINISHED)
-      setTimeout(() => setStatus(null), 1000) // This could be removed and let user to handle close
-    })
   }
 
   function closeReservationModal() {
@@ -39,23 +17,11 @@ export default function BookAppointment() {
   return (
     <div className="z-[1] w-full lg:w-1/2">
       <HeadingText />
-
       <Button onClick={handleOnClick}>Agendar cita</Button>
 
       {showModal ? (
-        <ReservationModal
-          onReservationSubmit={onReservationFormSubmit}
-          closeModal={closeReservationModal}
-        ></ReservationModal>
+        <ReservationModal closeModal={closeReservationModal}></ReservationModal>
       ) : null}
-
-      {status === reservationStates.FINISHED ? (
-        <ReservationStatusModal></ReservationStatusModal>
-      ) : status === null ? null : (
-        <ModalWrapper>
-          <Loader />
-        </ModalWrapper>
-      )}
     </div>
   )
 }
--- patagon-barber-frontend/src/components/ReservationModal/index.tsx
+++ patagon-barber-frontend/src/components/ReservationModal/index.tsx
@@ -1,189 +1,57 @@
-import { DatePicker, TimePicker } from '@mui/x-date-pickers'
-import dayjs, { Dayjs } from 'dayjs'
-import { ChangeEvent, FormEvent, useContext } from 'react'
-import { FormInput } from '../FormInput'
-import { ModalWrapper } from '../ModalWrapper'
-import { ModalClosingButton } from '../ModalClosingButton'
+import { useContext, useEffect, useState } from 'react'
+import ReservationForm from './ReservationForm'
 import { ReservationContext } from '../../context/reservationContext'
+import { setAppointment } from '../../services/appointmentServices'
+import { ReservationStatusModal } from '../ReservationStatusModal'
+import { ModalWrapper } from '../ModalWrapper'
+import { Loader } from '../Loader/Loader'
 
-const defaultDate = dayjs().hour(15).minute(0)
+enum reservationStates {
+  'STORING',
+  'FINISHED',
+  'COMPLETING'
+}
 
-// const initialClientState: Client = {
-//   id: undefined,
-//   name: '',
-//   lastname: '',
-//   email: '',
-//   phone_number: ''
-// }
-// const initialAppointmentState: Appointment = {
-//   id: undefined,
-//   client: undefined,
-//   date: '',
-//   time: '',
-//   service: undefined
-// }
-
-const ReservationModal = ({
-  onReservationSubmit,
+export default function ReservationModal({
   closeModal
 }: {
-  onReservationSubmit: () => void
   closeModal: () => void
-}) => {
-  // const [clientData, setClientData] = useState<Client>(initialClientState)
-  // const [appointmentData, setAppointmentData] = useState<Appointment>(
-  //   initialAppointmentState
-  // )
-
+}) {
+  const [showForm, setShowForm] = useState(true)
+  const [status, setStatus] = useState<reservationStates | null>(
+    reservationStates.COMPLETING
+  )
   const reservation = useContext(ReservationContext)
 
-  const disabledDates = ['2024-05-15', '2024-05-11', '2024-05-19']
+  useEffect(() => {
+    if (reservation?.appointment === undefined) return
 
-  function shouldDisableDate(date: Dayjs) {
-    const dateString = date.toISOString().split('T')[0]
-    return disabledDates.includes(dateString)
-  }
+    if (status === reservationStates.STORING) {
+      setAppointment(reservation.appointment).then(() => {
+        setStatus(reservationStates.FINISHED)
+        setTimeout(() => closeModal(), 1000) // This could be removed and let user to handle close
+      })
+    }
+  }, [status])
 
-  // function shouldDisableTime(dateTime: Dayjs) {
-  //   const hour = Number(dateTime.toISOString().split('T')[1].split(':')[0])
-
-  //   return false
-  // }
-
-  function onClientNameChange(event: ChangeEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    const { ...props } = reservation?.client
-    reservation?.updateClient({
-      ...props,
-      name: newValue
-    })
-  }
-
-  function onClientLastnameChange(event: ChangeEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    const { ...props } = reservation?.client
-    reservation?.updateClient({
-      ...props,
-      lastname: newValue
-    })
-  }
-
-  function onClientEmailChange(event: ChangeEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    const { ...props } = reservation?.client
-    reservation?.updateClient({
-      ...props,
-      email: newValue
-    })
-  }
-
-  function onClientPhoneNumberChange(event: ChangeEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    const { ...props } = reservation?.client
-    reservation?.updateClient({
-      ...props,
-      phone_number: newValue
-    })
-  }
-
-  function onDateAccept(date: Dayjs | null) {
-    if (date === null) return // Here must be a feedback to user
-
-    const settledDate = dayjs(date).format('YYYY-MM-DD HH:00')
-    const dateValue = settledDate.split(' ')[0]
-
-    const { ...props } = reservation?.appointment
-    reservation?.updateAppointment({
-      ...props,
-      date: dateValue
-    })
-  }
-
-  function onTimeAccept(time: Dayjs | null) {
-    if (time === null) return // Here must be a feedback to user
-
-    const settledDate = dayjs(time).format('YYYY-MM-DD HH:00')
-    const timeValue = settledDate.split(' ')[1]
-
-    const { ...props } = reservation?.appointment
-    reservation?.updateAppointment({
-      ...props,
-      time: timeValue
-    })
-  }
-
-  function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    onReservationSubmit()
-    closeModal()
+  function onReservationFormSubmit() {
+    setStatus(reservationStates.STORING)
+    setShowForm(false)
   }
 
   return (
     <ModalWrapper>
-      <form
-        onSubmit={handleFormSubmit}
-        className="bg-white flex flex-col gap-4 min-w-80 p-5 rounded-sm"
-      >
-        <ModalClosingButton onClick={closeModal} />
-        <FormInput
-          name="client-name"
-          label="Nombre"
-          placeholder="Alejandro"
-          required={true}
-          maxLength={30}
-          onChange={onClientNameChange}
-        />
-        <FormInput
-          name="client-lastname"
-          label="Apellido"
-          placeholder="Juarez"
-          required={true}
-          maxLength={30}
-          onChange={onClientLastnameChange}
-        />
-        <FormInput
-          name="client-email"
-          label="E-mail"
-          placeholder="alejandro96@gmail.com"
-          maxLength={40}
-          onChange={onClientEmailChange}
-        />
-        <FormInput
-          name="client-phone-number"
-          label="Telefono"
-          placeholder="2966484800"
-          required={true}
-          maxLength={40}
-          onChange={onClientPhoneNumberChange}
-        />
-        <DatePicker
-          defaultValue={defaultDate}
-          disablePast={true}
-          showDaysOutsideCurrentMonth={false}
-          shouldDisableDate={shouldDisableDate}
-          onAccept={onDateAccept}
-          views={['year', 'month', 'day']}
-          maxDate={defaultDate.endOf('month')}
-          minDate={defaultDate.startOf('month')}
-        ></DatePicker>
-        <TimePicker
-          defaultValue={defaultDate}
-          disablePast={true}
-          maxTime={dayjs().set('hour', 20)}
-          minTime={dayjs().set('hour', 15)}
-          ampm={false}
-          ampmInClock={false}
-          minutesStep={60}
-          onAccept={onTimeAccept}
-        ></TimePicker>
-        <input
-          type="submit"
-          className="py-2 px-5 mt-1 bg-cyan-600 text-white font-bold rounded-md "
-          value="Agendar"
-        />
-      </form>
+      {showForm ? (
+        <ReservationForm
+          onReservationSubmit={onReservationFormSubmit}
+          closeModal={closeModal}
+        ></ReservationForm>
+      ) : null}
+      {status === reservationStates.FINISHED ? (
+        <ReservationStatusModal></ReservationStatusModal>
+      ) : status === reservationStates.STORING ? (
+        <Loader />
+      ) : null}
     </ModalWrapper>
   )
 }
-
-export default ReservationModal
