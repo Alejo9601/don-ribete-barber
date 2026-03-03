5ab5d677cbecb5e2b1253d5d3714377b44e19372
Alejo9601 <alejandro96jm@gmail.com>
2026-03-03 15:36:20 -0300

    feat(frontend): refine gallery navigation links

--- frontend/src/components/Header/Navbar/SectionLink.tsx
+++ frontend/src/components/Header/Navbar/SectionLink.tsx
@@ -8,7 +8,7 @@ const SectionLink = ({
   refTo?: string
 }) => {
   return (
-    <li className="cursor-pointer p-5 text-xl text-stone-200 transition hover:text-white lg:p-0 lg:text-sm lg:uppercase lg:tracking-[0.18em]">
+    <li className="cursor-pointer p-5 text-xl text-stone-200 transition hover:text-sky-300 lg:p-0 lg:text-sm lg:uppercase lg:tracking-[0.18em]">
       <Link href={refTo}>{children}</Link>
     </li>
   )
