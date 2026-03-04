845d6f65142726ffb20b8e1e755322d9ee831fcf
Alejo9601 <alejandro96jm@gmail.com>
2026-03-04 15:16:40 -0300

    style(frontend): reduce spacing below embedded map

--- frontend/src/components/EmbeddedMap/index.tsx
+++ frontend/src/components/EmbeddedMap/index.tsx
@@ -2,7 +2,7 @@ import { WithMarginWrapper } from '../WithMarginWrapper'
 
 export function EmbeddedMap() {
   return (
-    <WithMarginWrapper customClasses="flex flex-col gap-8 pb-16 sm:gap-10 lg:flex-row lg:gap-12 lg:pb-20">
+    <WithMarginWrapper customClasses="flex flex-col gap-8 pb-5 sm:gap-10 sm:pb-6 lg:flex-row lg:gap-12 lg:pb-8">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2479.4310768916516!2d-72.21199782352558!3d-51.57866237182961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdb01c6d3faa2f8f%3A0x2cc5b76ab74c6897!2sAv.%20Circunvalacion%2025%20de%20Mayo%20458%2C%2028%20de%20Noviembre%2C%20Santa%20Cruz!5e0!3m2!1ses-419!2sar!4v1715868408860!5m2!1ses-419!2sar"
         className="h-80 w-full rounded-[1.5rem] lg:h-[450px] lg:w-[600px]"
